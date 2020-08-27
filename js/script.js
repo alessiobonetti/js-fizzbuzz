555d3b9423d153549151303f1acea83a60b898dd
Alessio Bonetti <alesine87@gmail.com>
2020-08-27 10:11:27 +0200

    fizz buzz

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@ var numeri = [];
 // Numeri da 1 a 100
 for (i=1; i<=100; i++){
   numeri.push(i);
+  var posizioneNumero = numeri.indexOf(i);
   if  (i % 3 == 0 && i % 5 == 0 ) {
-    var posizioneNumero = numeri.indexOf(i);
     numeri[posizioneNumero] = "FizzBuzz";
   }
   else if (i % 5 == 0) {
-    var posizioneNumero = numeri.indexOf(i);
     numeri[posizioneNumero] = "Buzz";
   }
   else if(i % 3 == 0) {
-    var posizioneNumero = numeri.indexOf(i);
     numeri[posizioneNumero] = "Fizz";
   }
 }
-console.log(numeri);
+
+document.getElementById("risultato").innerHTML = numeri
